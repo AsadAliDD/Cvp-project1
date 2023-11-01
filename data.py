65c0917211673f8e7a0db0377d7b0f9e99d8459f
Asad Ali <asadalisps@gmail.com>
2023-11-01 22:30:05 +0100

    added script to generate objects and rescale

--- data.py
+++ data.py
@@ -2,19 +2,25 @@ import objaverse
 import multiprocessing
 import warnings
 import random
-
+import json
+import pandas as pd
 
 
 def select_objects(seed,object_type,object_number):
     lvis_annotations = objaverse.load_lvis_annotations()
-    selected_uids=[]
+    labels=[]
+    uids=[]
     random.seed(seed)
     keys=random.choices(list(lvis_annotations.keys()),k=object_type)
     for key in keys:
         obj_uid=lvis_annotations[key]
-        selected_uids.extend(random.choices(obj_uid,k=object_number))
-    print(keys)
-    return selected_uids
+        uids.extend(random.choices(obj_uid,k=object_number))
+        label=[key]*object_number
+        labels.extend(label)
+        
+
+    df=pd.DataFrame({'Labels':labels,'Uids':uids})
+    return df
 
 
 def download_objects(uids):
@@ -26,13 +32,17 @@ def download_objects(uids):
     )
     return objects
 
+def get_annotations(uids):
+    anon=objaverse.load_annotations(uids)
+    return anon
+
 
 def main():
 
 
 
-    uids=select_objects(seed=10,object_type=50,object_number=2)
-    objects=download_objects(uids)
+    selected_objects=select_objects(seed=10,object_type=50,object_number=2)
+    # objects=download_objects(uids)
     # print (objects)
 
 if __name__ == '__main__':
